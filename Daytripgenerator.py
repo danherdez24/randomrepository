67ba751f31c6e34ca4e0543759fa62951466bbf0
mac <mac@macs-MacBook-Air.local>
2022-12-21 15:55:25 -0800

    Added 8 lines to Daytripgenerator.py

---
+++ Daytripgenerator.py
@@ -0,0 +1,8 @@
+
+Destinations = ["California", "Texas", "Alaska", "Nevada"]
+
+Restaurants = ["In N Out", "Buccees", "Ginger", "Hell's Kitchen"]
+
+Transportation = ["Airplane", "Car", "Cruise", "Bus"]
+
+Entertainment = ["Hiking", "Spa Day", "Gun Range", "Gambling"]
